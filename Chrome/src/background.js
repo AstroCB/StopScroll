f6b00fc9b85fd8e927919375f8127207d8b7779c
Cameron Bernhardt <cambernhardt@me.com>
2018-01-14 21:24:06 -0500

    Update Chrome extension to use menu icon as well (fix #1)

--- Chrome/src/background.js
+++ Chrome/src/background.js
@@ -1,31 +1,46 @@
 var paused = false; // Global sentinel
 
+// Pause items for context menu & toolbar icon
 function addPause() {
     chrome.contextMenus.create({
         "title": "Pause Scrolling",
         "id": "pause",
     });
+
+    chrome.contextMenus.create({
+        title: "Pause Scrolling",
+        contexts: ["browser_action"],
+        "id": "menu_pause"
+    });
 }
 
-addPause();
-
+// Resume menus for context menu & toolbar icon
 function addResume() {
     chrome.contextMenus.create({
         "title": "Resume Scrolling",
         "id": "resume",
     });
+
+    chrome.contextMenus.create({
+        "title": "Resume Scrolling",
+        "contexts": ["browser_action"],
+        "id": "menu_resume"
+    });
 }
 
+// Toggle between paused & resumed
 function handleClick() {
     paused = !paused;
 
     if (paused) {
         sendMessage(true);
         chrome.contextMenus.remove("pause");
+        chrome.contextMenus.remove("menu_pause");
         addResume();
     } else {
         sendMessage(false);
         chrome.contextMenus.remove("resume");
+        chrome.contextMenus.remove("menu_resume");
         addPause();
     }
 }
@@ -35,11 +50,14 @@ chrome.contextMenus.onClicked.addListener(handleClick);
 // Send pause message to injected script
 function sendMessage(shouldPause) {
     chrome.tabs.query({
-        active: true,
-        currentWindow: true
+        "active": true,
+        "currentWindow": true
     }, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {
-            pause: shouldPause
+            "pause": shouldPause
         });
     });
 }
+
+// Add the pause items to start
+addPause();
